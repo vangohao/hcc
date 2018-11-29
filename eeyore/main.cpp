d80d191613f554f49594fc6590c39796bb4dbc09
Vango Hao <vangohao@gmail.com>
2018-11-29 13:07:51 +0800

    1

--- eeyore/main.cpp
+++ eeyore/main.cpp
@@ -42,25 +42,6 @@ int main(int argc, char * args[])
     fclose(yyout);
     #endif
     #ifndef __LOCAL_HAO
-/*     FILE* g = fopen("tmp.oi","r");
-    char nn[100];
-    fscanf(g,"%s",nn);
-    fclose(g);
-    int n;
-    n = atoi(nn);
-    n++;
-    strcat(nn,".out");
-    FILE* f = fopen(nn,"w");
-    pred(stdin,f);
-    fclose(f);
-    f = fopen(nn,"r");
-    yyin = f;
-    yyout = stdout;
-    yyparse();
-    fclose(f);
-    g  = fopen("tmp.oi","w");
-    fprintf(g,"%d",n);
-    fclose(g); */
     yyin = stdin;
     yyout = stdout;
     yyparse();
