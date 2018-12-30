d505a97cbb3f9b0be4a73ac3040c786300418db6
Vango Hao <vangohao@gmail.com>
2018-12-31 01:26:45 +0800

    更换tigger默认生成tg

--- tigger/main.cpp
+++ tigger/main.cpp
@@ -7,8 +7,8 @@ int target;
 int main(int argc , char** argv)
 {
     yyparse();
-    target = 1;
-    if(argc>1 && strcmp(argv[1],"-tg") == 0) target = 0;
+    target = 0;
+    if(argc>1 && strcmp(argv[1],"-m64") == 0) target = 1;
     else if(argc>1 && strcmp(argv[1],"-m32")==0)  target = 2;
     AnalyzInstance.process();
     return 0;
